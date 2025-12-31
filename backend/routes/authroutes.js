a297bccb480bf6264c2ba34d634cbbb0ae34ba7d
Deepak H Kemmanakeri <127978063+DeepakHK24@users.noreply.github.com>
2025-12-31 12:41:01 +0530

    Backend

---
+++ backend/routes/authroutes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { registerUser, loginUser } = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/authMIddleware");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+// ONLY ADMIN
+router.get(
+  "/admin",
+  authMiddleware,
+  roleMiddleware("admin"),
+  (req, res) => {
+    res.json({ message: "Welcome Admin" });
+  }
+);
+
+// ONLY DOCTOR
+router.get(
+  "/doctor",
+  authMiddleware,
+  roleMiddleware("doctor"),
+  (req, res) => {
+    res.json({ message: "Welcome Doctor" });
+  }
+);
+
+module.exports = router;
